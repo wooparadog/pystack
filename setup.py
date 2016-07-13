2c59473aada841daf7a9e660f2fe9d8b148e362c
WooParadog <haochuan.guo@ele.me>
2016-07-13 17:39:55 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ entry_points = ["pstack = stack:stack"]
 
 setup(
     name="pstack",
-    version='0.4',
+    version='0.5',
     description="Tool to print python thread and greenlet stacks",
     author="Haochuan Guo",
     author_email="guohaochuan@gmail.com",
